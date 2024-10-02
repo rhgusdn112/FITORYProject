6d0f66bff4e0d86a0bdc735bd7776091e11ec42f
대현009 <hanmir009@naver.com>
2024-10-02 17:48:32 +0900

    20241002 login페이지 커밋

--- src/main/java/edu/kh/fit/main/controller/Maincontroller.java
+++ src/main/java/edu/kh/fit/main/controller/Maincontroller.java
@@ -1,7 +1,10 @@
 package edu.kh.fit.main.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.kh.fit.main.service.MainService;
 import lombok.RequiredArgsConstructor;
@@ -10,9 +13,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/")
 public class Maincontroller {
-	
-	private final MainService service;
-	
-	
-	
+   
+   private final MainService service;
+   
+ @GetMapping("login")
+ public String login() {
+    return "login/login";
+ }
+   
+
+
 }
