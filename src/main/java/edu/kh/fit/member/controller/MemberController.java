477b2f699e4397d0d130d03604a353433a30198e
sangmin-seong <money679@naver.com>
2024-10-02 17:00:49 +0900

    Main 관련 java 파일

--- src/main/java/edu/kh/fit/member/controller/MemberController.java
+++ src/main/java/edu/kh/fit/member/controller/MemberController.java
@@ -24,7 +24,7 @@ public class MemberController {
 	
 	private final MemberService service;
 	
-	/** 로그인
+	/** (회원) 로그인 서비스
 	 * @param memberEmail
 	 * @param memberPw
 	 * @param saveEmail
@@ -49,40 +49,22 @@ public class MemberController {
 			ra.addFlashAttribute("message", "해당하는 정보가 없습니다.");
 		}else { // 로그인 성공
 
-					// loginMember를 session scope에 추가
-					// 방법 1) HttpSession 이용
-					// 방법 2) @SessionAttirbutes + Model 이용 방법
-			
-			/* Model을 이용해서 Session scope에 값 추가하는 방법 */
-			// 1. model에 값 추가
 			model.addAttribute("memberLogin", memberLogin);
-			
-			// 2. 클래스 선언부 위에 @SessionAttributes({"key"}) 추가
-			//  -> key 값은 model에 추가된 key 값 "loginMember" 작성
-			//   (request -> session)
-			
-			// @SessionAttributes :
-			// Model에 추가된 값 중 session scope로 올리고 싶은 값의
-			// key를 작성하는 어노테이션
 			
 			//----------------------------------------------------------------
 			/* 이메일 저장 코드(Cookie) */
 			
-			// 1. Cookie 객체 생성
 			Cookie cookie = new Cookie("saveEmail", memberEmail);
 			
-			// 2. 만들어진 Cookie가 사용된 경로(url)
-			cookie.setPath("/"); // localhost 이하 모든 주소
+			cookie.setPath("/"); 
 			
-			// 3. Cookie가 유지되는 시간(수명) 설정
-			if(saveEmail == null) { // check X
-				cookie.setMaxAge(0); // 만들어지자마자 만료
-														 // == 기존에 만들어진 쿠키가 있으면 덮어씌우고 없어짐 
-			}else { // check O
-				cookie.setMaxAge(60 * 60 * 24 * 30); // 초단위로 작성
+			if(saveEmail == null) { 
+				cookie.setMaxAge(0); 
+														  
+			}else { 
+				cookie.setMaxAge(60 * 60 * 24 * 30); 
 			}
 		
-			// 4. resp 객체에 추가해서 클라이언트에 전달
 			resp.addCookie(cookie);
 			
 			
@@ -90,6 +72,6 @@ public class MemberController {
 		}
 		
 		
-		return "redirect:/"; // 메인페이지 리다이렉트
+		return "redirect:/"; 
 	}
 }
