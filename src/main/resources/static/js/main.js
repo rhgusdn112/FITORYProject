ce0d4f48534cd7e3305359b44b622ea185a8bbff
대현009 <hanmir009@naver.com>
2024-10-04 17:50:59 +0900

    20241004 메인 페이지, 헤더, 푸터 작성 중.

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menuIcon = document.querySelector('.menu-icon');
+  menuIcon.addEventListener('click', () => {
+      alert("메뉴 클릭됨!");
+  });
+});
