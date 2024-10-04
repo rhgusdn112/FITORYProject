0109dc8750e78c3f9dffd683bacb5573a07fa26c
sangmin-seong <money679@naver.com>
2024-10-04 16:21:01 +0900

    각 html 파일에 코드작성 부탁드립니다.

--- src/main/java/edu/kh/fit/main/controller/Maincontroller.java
+++ src/main/java/edu/kh/fit/main/controller/Maincontroller.java
@@ -1,28 +1,39 @@
 package edu.kh.fit.main.controller;
 
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.kh.fit.main.service.MainService;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/")
 public class Maincontroller {
-
 	private final MainService service;
 	
 	@GetMapping("login")
-	public String login() {
-	  return "login/login";
- }  
+ 	public String login() {
+	 	return "login/login";
+ 	}
 	
-	@GetMapping("signUp")
-	public String signUp() {
-		return "common/signUp";
-	}
+ 	@GetMapping("signUp")
+ 	public String signUp() {
+ 		return "login/signUp";
+ 	}
+ 
+ 	@GetMapping("find-id")
+ 	public String findId() {
+ 		return "login/findId";
+ 	}
+ 	
+ 	@GetMapping("find-password")
+ 	public String findPw() {
+ 		return "login/findPw";
+ 	}
+
 }
 
