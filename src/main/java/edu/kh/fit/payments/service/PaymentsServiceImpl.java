8a1ce08d8d56049d20dbb004986bd0532494b61c
MAX-G <go1991woo@naver.com>
2024-10-18 17:44:48 +0900

    20241018 결제수단 폴더 구조 설정만 완료

---
+++ src/main/java/edu/kh/fit/payments/service/PaymentsServiceImpl.java
@@ -0,0 +1,11 @@
+package edu.kh.fit.payments.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentsServiceImpl implements PaymentsService {
+
+}
