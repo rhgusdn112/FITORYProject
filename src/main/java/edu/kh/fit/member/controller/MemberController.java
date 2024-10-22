74b94adeeb349e6fe3eb93966f67b049828166f9
sangmin-seong <money679@naver.com>
2024-10-22 12:29:17 +0900

    login페이지 로그인 시 redirect하여 session에서 사용가능하게 수정

--- src/main/java/edu/kh/fit/member/controller/MemberController.java
+++ src/main/java/edu/kh/fit/member/controller/MemberController.java
@@ -5,23 +5,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.fit.member.dto.Member;
 import edu.kh.fit.member.service.MemberService;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Controller
-@SessionAttributes("member")
+@RequestMapping("member")
+@SessionAttributes("memberLogin")
 @RequiredArgsConstructor
 public class MemberController {
 	
@@ -38,7 +36,7 @@ public class MemberController {
 	 */
 	@PostMapping("login")
 	public String memberLogin(
-			@RequestParam("username") String memberEmail,
+			@RequestParam("email") String memberEmail,
 			@RequestParam("password")		 String memberPw,
 			@RequestParam(name = "saveEmail", required = false) String saveEmail,
 			Model model,
@@ -53,24 +51,8 @@ public class MemberController {
 												"아이디 혹은 패스워드가 일치하지 않습니다.");
 		}else {
 			model.addAttribute("memberLogin", memberLogin);
-			
-			//----------------------------------------------------------------
-			/* 이메일 저장 코드(Cookie) */
-			Cookie cookie = new Cookie("saveEmail", memberEmail);
-			
-			cookie.setPath("/"); 
-			
-			if(saveEmail == null) { 
-				cookie.setMaxAge(0); 
-														  
-			}else { 
-				cookie.setMaxAge(60 * 60 * 24 * 30); 
-			}
-		
-			resp.addCookie(cookie);
-			//----------------------------------------------------------------
 		}
-		return "redirect:/"; 
+		return "redirect:/main"; 
 	}
 	
 	/** 로그아웃
