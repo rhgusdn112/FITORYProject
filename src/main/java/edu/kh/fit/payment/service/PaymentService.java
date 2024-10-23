7caa3f78856c6d43bd0aa2965b5476784a182324
MAX-G <go1991woo@naver.com>
2024-10-23 18:20:43 +0900

    241023 Payment 경로구성 수정. 주문정보와 결제정보 업데이트 생성

--- src/main/java/edu/kh/fit/payment/service/PaymentService.java
+++ src/main/java/edu/kh/fit/payment/service/PaymentService.java
@@ -3,6 +3,13 @@ package edu.kh.fit.payment.service;
 import edu.kh.fit.payment.dto.Payment;
 
 public interface PaymentService {
-    Payment selectPaymentClass(int boardNo);
-    void updatePaymentStatus(int boardNo, String status);
+    
+	// 주문 정보 조회
+	Payment selectOrderInfo(int orderNo);
+    
+	// 결제 상태 업데이트
+    void updatePaymentStatus(int orderNo, String status);
+    
+    // 결제 요청
+	Payment selectPaymentClass(int orderNo);
 }
