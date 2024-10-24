64c649a09909db348ec7664fe9f3a81da28c0835
sangmin-seong <money679@naver.com>
2024-10-24 16:06:27 +0900

    admin 로그인 페이지 연결

--- src/main/java/edu/kh/fit/admin/controller/AdminController.java
+++ src/main/java/edu/kh/fit/admin/controller/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.fit.admin.dto.Admin;
@@ -18,6 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping("admin")
 @RequiredArgsConstructor
+@SessionAttributes("adminLogin")
 public class AdminController {
 	
 	private final AdminService service;
