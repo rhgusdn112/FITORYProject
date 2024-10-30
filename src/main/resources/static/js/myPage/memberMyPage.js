3729ff5a5ebb971f6ade7776ff9c308d14994cef
L-sj99 <yms6897@naver.com>
2024-10-30 18:01:25 +0900

    이모저모 2번째

--- src/main/resources/static/js/myPage/memberMyPage.js
+++ src/main/resources/static/js/myPage/memberMyPage.js
@@ -2,16 +2,16 @@ const myPageInfo = document.querySelector("#myPageInfo");
 const updateBtn = document.querySelector("#updateBtn");
 
 const checkObj = {
-  "memberName" : true, "memberTel" : true
+  "memberName": true, "memberTel": true
 }
 const update = document.querySelector("#update");
 update?.addEventListener("submit", e => {
-  for(let key in checkObj) { 
-      if(checkObj[key] === false) { // 닉네임, 전화번호 중 유효하지 않은 값이 있을 경우
+  for (let key in checkObj) {
+    if (checkObj[key] === false) { // 닉네임, 전화번호 중 유효하지 않은 값이 있을 경우
       let str = " 유효하지 않습니다.";
-      switch(key){
-        case "memberName" : str = "이름이" + str; break;
-        case "memberTel" : str = "전화번호가" + str; break;
+      switch (key) {
+        case "memberName": str = "이름이" + str; break;
+        case "memberTel": str = "전화번호가" + str; break;
       }
       alert(str);
       e.preventDefault();
@@ -21,11 +21,8 @@ update?.addEventListener("submit", e => {
   }
 });
 /* checkbox, 버튼 요소 얻어오기 */
-const checkbox = document.querySelector("#checkbox");
-const seeBtn = document.querySelector("#seeBtn");
 const deleteBtn = document.querySelector("#deleteBtn");
-checkbox.addEventListener("")
-
+const checkbox = document.querySelectorAll('input[type="checkbox"]');
 deleteBtn.addEventListener("click", () => {
 
 })
