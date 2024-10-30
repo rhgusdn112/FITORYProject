650d44da79a3b9142bc9d1730dbce85414fb539d
L-sj99 <yms6897@naver.com>
2024-10-30 13:43:53 +0900

    점심 풀리퀘스트

--- src/main/resources/static/js/trainer/trainer.js
+++ src/main/resources/static/js/trainer/trainer.js
@@ -2,16 +2,16 @@
 // ----------------------------------------------------------------------------------------------
 /* 강사 정보 상세조회 */
 const url = "/trainer/trainerDetail/{trainerNo:[0-9]+}";
-const trainerDetailNoPattern = /^\/trainer\/trainerDetail\/([0-9]+)$/;
+const trainerDetailNo = /^\/trainer\/trainerDetail\/([0-9]+)$/;
 
 // 정규식을 사용하여 URL에서 trainerNo 부분 추출
-const match = url.match(trainerDetailNoPattern);
+const match = url.match(trainerDetailNo);
 
 // 'watch' 버튼 클릭 시 처리
 const watchBtn = document.getElementById("watch");
 if (watchBtn) {
   watchBtn.addEventListener("click", () => {
-    const trainerNoElement = document.getElementById('trainerName');
+    const trainerNoElement = document.getElementById('trainerNickname');
     const trainerNo = trainerNoElement ? trainerNoElement.getAttribute('data-trainerNo') : null;
     if (match) {
       const trainerNoFromUrl = match[1];
