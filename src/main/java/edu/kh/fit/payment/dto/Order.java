51c4e5bdfb0cd38807181df30c4a3eacdb34148e
MAX-G <go1991woo@naver.com>
2024-10-31 09:35:47 +0900

    Reapply "payment 경로 오류가 있어 연결하는 값 수정."
    This reverts commit 1a48252658696117329ee15a0b17791db515e4bb.

--- src/main/java/edu/kh/fit/payment/dto/Order.java
+++ src/main/java/edu/kh/fit/payment/dto/Order.java
@@ -21,10 +21,11 @@ public class Order {
 	private int orderMemberNo; // 주문자 번호
 	private int orderClassNo; // 강의 번호
 	
-	private String title;
+	private String classTitle;
 	private String detail;
 	private String trainerNickname;
 	private String boardTypeName;
 	private String paymentDate; // 결제일
+	private String customerName;
 	
 }
